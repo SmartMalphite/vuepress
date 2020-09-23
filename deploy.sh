ca024f9ac4cdc96e3538eaa05e992e5178279280
Enbo Wang <enbo.wang@shopee.com>
2020-09-23 18:04:26 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@
 set -e
 
 # build
-npm run build
+yarn run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+# git init
+git add --all
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:enbo-wang/vuepress.git master:gh-pages
 
-cd -
+# cd -
